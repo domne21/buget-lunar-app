0f42c87ca92dd0a0acdf746e591f70b855a39910
Daniel Constantin Domnescu <domnescudaniel@gmail.com>
2019-02-02 11:57:39 +0200

    added date+categories input method

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,31 @@
-function loadExpenses() {
-    $.ajax('data/expenses.json').done(function(expenses){
-        console.info('contacts loaded', expenses);
-        window.globalExpenses = expenses;
-        displayContacts(expenses);
+function load() {
+    $.ajax('data/transactions.json').done(function(transactions){
+        console.info('transactions loaded', transactions);
+        window.globalTransactions = transactions;
+        display(transactions);
     });
 }
+
+function getNewRow() {
+    return `<tr>
+            <td><input type="date" name="date" placeholder="Date"/></td>
+            <td><input type="text" list="categories" name="categories" placeholder="Categories"/></td>
+            <td><input type="number" step="0.01" name="ammount" placeholder="Ammount"/></td>
+        </tr>`;
+}
+
+function display(transactions) {
+    var rows = transactions.map(function(transaction) {
+        return `<tr>
+            <td>${transaction.date}</td>
+            <td>${transaction.categories}</td>
+            <td>${transaction.ammount} RON </td>
+        </tr>`;
+    });
+    var actions = getNewRow();
+    rows.push(actions);
+
+    document.querySelector('tbody').innerHTML = rows.join('');
+}
+
+load();
