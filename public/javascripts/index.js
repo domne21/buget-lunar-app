96c560bbfb7950649bf7856effb9faee38f2d465
Daniel Constantin Domnescu <domnescudaniel@gmail.com>
2019-02-02 14:38:53 +0200

    added delete transaction

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,14 +14,20 @@ function save() {
     var date = document.querySelector('input[name=date]').value;
     var categories = $('input[name=categories]').val();
     var ammount = $('input[name=ammount]').val();
-    console.debug('saving...', date, categories, ammount);
+    var transId = new Date().getTime();
+    console.debug('saving...', date, categories, ammount, transId);
     
     $.post('/transactions/create', {
         date,
         categories,
-        ammount
+        ammount,
+        transId
     }).done(function(response){
         console.warn('done creating transaction', response);
+        if (response.success) {
+            console.info('reloading..')
+            load();
+        }
     });
 }
 
@@ -31,6 +37,10 @@ function display(transactions) {
             <td>${transaction.date}</td>
             <td>${transaction.categories}</td>
             <td>${transaction.ammount} RON </td>
+            <td>
+                <a href="/transactions/delete?transId=${transaction.transId}">&#10006;</a>
+                <a href="#" class="edit" data-id="${transaction.transId}">&#9998;</a>
+            </td>
         </tr>`;
     });
     var actions = getNewRow();
@@ -39,4 +49,23 @@ function display(transactions) {
     document.querySelector('tbody').innerHTML = rows.join('');
 }
 
+function initEvents() {
+    // TODO use native click
+    $("tbody").delegate( "a.edit", "click", function() {
+        transToEdit = this.getAttribute('data-id');
+
+        var transaction = globalTransactions.find(function(transaction){
+            return transaction.transId == transToEdit;
+        });
+        console.log('edit', transToEdit, transaction);
+        
+        document.querySelector('input[name=date]').value = transaction.date;
+        $('input[name=categories]').val(contact.categories);
+        $('input[name=ammount]').val(contact.ammount);
+        $('input[name=transId]').val(contact.transId);
+
+    });
+}
+
+initEvents();
 load();
--- routes/transactions.js
+++ routes/transactions.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/* POST users listing. */
+// save
 router.post('/create', function (req, res, next) {
 
   var date = req.body.date;
   var categories = req.body.categories;
   var ammount = req.body.ammount;
+  var transId = req.body.transId;
 
   var content = fs.readFileSync('public/data/transactions.json');
   var transactions = JSON.parse(content);
@@ -16,15 +17,32 @@ router.post('/create', function (req, res, next) {
     date,
     categories,
     ammount,
+    transId
   });
 
-  content = JSON.stringify(transactions, null, 2);
+  content = JSON.stringify(transactions, null, 3);
   fs.writeFileSync('public/data/transactions.json', content);
 
-  res.json({ sucess: true });
+  res.json({ success: true });
+});
 
-  res.redirect('/index.html');
+// delete
+router.get('/delete', function (req, res, next) { 
+
+  var transId = req.query.transId;
+  var content = fs.readFileSync('public/data/transactions.json');
+  var transactions = JSON.parse(content);
+
+  var remainingTransactions = transactions.filter(function (transaction) {
+    return transaction.transId != transId;
+  });
+
+  content = JSON.stringify(remainingTransactions, null, 3);
+  fs.writeFileSync('public/data/transactions.json', content);
+
+  res.redirect('/index.html');;
 
 });
 
 module.exports = router;
+
